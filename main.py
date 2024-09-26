95e22ba331cb34f5a7973425b1780be4bdb9e736
JohnyWalker167 <150372877+JohnyWalker167@users.noreply.github.com>
2024-09-26 20:50:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+from config import *
+from pyromod import listen
+from pyrogram.errors import FloodWait
+from pyrogram import Client, filters, enums
 
+
+app = Client(
+                "userbot",
+                api_id=int(API_ID),
+                api_hash=API_HASH,
+                session_string=STRING_SESSION,
+                no_updates = True
+)
+
+@app.on_message(filters.command("delete") & filters.user(OWNER_USERNAME))
+async def delete_command(client, message):
+    try:
+        await message.reply_text("Enter channel_id")
+        channel_id = int((await app.listen(message.chat.id)).text)
+
+        await message.reply_text("Enter count")
+        limit = int((await app.listen(message.chat.id)).text)
+
+        await app.send_message(channel_id, "Hi")
+
+        try:
+            async for message in user.get_chat_history(channel_id, limit):
+                await message.delete()
+        except Exception as e:
+            logger.error(f"Error deleting messages: {e}")
+        await user.send_message(channel_id, "done")
+    except Exception as e:
+        logger.error(f"Error : {e}")
+
+app.run()
